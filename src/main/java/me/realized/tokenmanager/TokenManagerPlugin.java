352dd5196e37bf6ad4eba5e1b157a6753a7dee20
SirBlobman <blobmansrealm@gmail.com>
2021-06-29 18:31:23 -0400

    Fix build errors, comment out non-working code.

--- src/main/java/me/realized/tokenmanager/TokenManagerPlugin.java
+++ src/main/java/me/realized/tokenmanager/TokenManagerPlugin.java
@@ -1,11 +1,21 @@
 package me.realized.tokenmanager;
 
-import com.google.common.collect.Lists;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalLong;
 import java.util.stream.Collectors;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.google.common.collect.Lists;
 import lombok.Getter;
 import me.realized.tokenmanager.api.TokenManager;
 import me.realized.tokenmanager.command.commands.TMCommand;
@@ -25,18 +35,6 @@ import me.realized.tokenmanager.util.Log;
 import me.realized.tokenmanager.util.NumberUtil;
 import me.realized.tokenmanager.util.Reloadable;
 import me.realized.tokenmanager.util.StringUtil;
-import org.bstats.bukkit.Metrics;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.inventivetalent.update.spiget.SpigetUpdate;
-import org.inventivetalent.update.spiget.UpdateCallback;
-import org.inventivetalent.update.spiget.comparator.VersionComparator;
 
 public class TokenManagerPlugin extends JavaPlugin implements TokenManager, Listener {
 
@@ -86,31 +84,32 @@ public class TokenManagerPlugin extends JavaPlugin implements TokenManager, List
         new TMCommand(this).register();
         new TokenCommand(this).register();
 
-        new Metrics(this);
+//        new Metrics(this);
 
         if (!configuration.isCheckForUpdates()) {
             return;
         }
 
-        final SpigetUpdate updateChecker = new SpigetUpdate(this, RESOURCE_ID);
-        updateChecker.setVersionComparator(VersionComparator.SEM_VER_SNAPSHOT);
-        updateChecker.checkForUpdate(new UpdateCallback() {
-            @Override
-            public void updateAvailable(final String newVersion, final String downloadUrl, final boolean hasDirectDownload) {
-                TokenManagerPlugin.this.updateAvailable = true;
-                TokenManagerPlugin.this.newVersion = newVersion;
-                Log.info("===============================================");
-                Log.info("An update for " + getName() + " is available!");
-                Log.info("Download " + getName() + " v" + newVersion + " here:");
-                Log.info(RESOURCE_URL);
-                Log.info("===============================================");
-            }
+//        final SpigetUpdate updateChecker = new SpigetUpdate(this, RESOURCE_ID);
+//        updateChecker.setVersionComparator(VersionComparator.SEM_VER_SNAPSHOT);
+//        updateChecker.checkForUpdate(new UpdateCallback() {
+//            @Override
+//            public void updateAvailable(final String newVersion, final String downloadUrl, final boolean hasDirectDownload) {
+//                TokenManagerPlugin.this.updateAvailable = true;
+//                TokenManagerPlugin.this.newVersion = newVersion;
+//                Log.info("===============================================");
+//                Log.info("An update for " + getName() + " is available!");
+//                Log.info("Download " + getName() + " v" + newVersion + " here:");
+//                Log.info(RESOURCE_URL);
+//                Log.info("===============================================");
+//            }
+//
+//            @Override
+//            public void upToDate() {
+//                Log.info("No updates were available. You are on the latest version!");
+//            }
+//        });
 
-            @Override
-            public void upToDate() {
-                Log.info("No updates were available. You are on the latest version!");
-            }
-        });
         getServer().getPluginManager().registerEvents(this, this);
     }
 
